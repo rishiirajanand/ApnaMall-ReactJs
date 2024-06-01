75208cddc3cbd6213c56afa4162c55828c213dc8
rishiirajanand <rishirajanand5@gmail.com>
2024-06-01 08:36:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,232 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen flex">
+        <nav className="w-[15%] py-4 h-full bg-zinc-100 flex flex-col items-center">
+          <a href="/create" className="px-3 py-2 border border-blue-300 text-blue-400 rounded text-sm font-semibold">
+            Add new Product
+          </a>
+          <hr className="w-[80%] my-5"/>
+          <h1 className="w-[80%] font-semibold text-2xl">Category</h1>
+          <ul className="w-[80%] mt-4">
+            <li className=" mb-2 flex items-center gap-2">
+              <span className="w-[13px] h-[13px] rounded-full bg-red-300"></span>
+              cat 1
+            </li>  
+            <li className=" mb-2 flex items-center gap-2">
+              <span className="w-[13px] h-[13px] rounded-full bg-green-300"></span>
+              cat 2
+            </li>  
+            <li className=" mb-2 flex items-center gap-2">
+              <span className="w-[13px] h-[13px] rounded-full bg-yellow-300"></span>
+              cat 3
+            </li>  
+          </ul>
+        </nav>
+
+        <div className="w-[85%] h-full p-5 flex felx-col flex-wrap gap-4">
+
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+          <div className="card w-[200px] h-[300px] rounded text-center border shadow-md p-2 bg-yellow-100">
+            <div className="w-full h-[65%] bg-zinc-100 mb-2 transition-all rounded overflow-hidden">
+              <img
+                className="w-full h-full"
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" 
+                alt="bag" 
+              />
+            </div>
+            <h1>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </h1>
+          </div>
+
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
