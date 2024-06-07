74f32e8c340bd041611c67da5491655f6ae6a217
rishiirajanand <rishirajanand5@gmail.com>
2024-06-07 14:52:57 +0530

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
         setFilterCategory(products)
     }
     if (category != 'undefined') {
-      // getProductCategory();
+      getProductCategory();
       setFilterCategory(products.filter(p => p.category == category))
     }
   }, [category, products]);
